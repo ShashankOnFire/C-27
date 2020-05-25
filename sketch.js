213f54d55fb2a6bf766ac428b46b77dc55cfe682
ShashankOnFire <leelashashank2004@gmail.com>
2020-05-25 16:55:45 +0530

    I wrote the conept of constraint bodies

--- sketch.js
+++ sketch.js
@@ -1,10 +1,12 @@
 const Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
 
 var engine, world;
 var box1, pig1;
 var backgroundImg,platform;
+var cLog;
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -14,6 +16,7 @@ function setup(){
     var canvas = createCanvas(1200,400);
     engine = Engine.create();
     world = engine.world;
+	cLog = new Log(230,180,80,PI/2);
 
 
     ground = new Ground(600,height,1200,20);
@@ -35,6 +38,14 @@ function setup(){
     log5 = new Log(870,120,150, -PI/7);
 
     bird = new Bird(100,100);
+	var options = {
+		bodyA :bird,body,
+		bodyB : cLog.body,
+		stiffness:0.04,
+		length : 10
+	}
+    var chain = Constraint.create(options)
+	World.add(World.chain);
 
 }
 
@@ -61,4 +72,7 @@ function draw(){
 
     bird.display();
     platform.display();
+	cLog.display();
+	strokeWeight(3);
+	line(bird.body.position.x, bird.body.position.y.cLog.body.position.x, cLog.position.y);
 }
